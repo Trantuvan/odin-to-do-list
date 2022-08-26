f936bf970066f1a3428235cee6a185936c907c21
trantuvan <trantuvan.kan@gmail.com>
2022-08-26 09:14:07 +0700

    Check localStorage availability

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,21 @@ import "../scss/style.scss";
 import Logo from "../img/favicon-32x32.png";
 
 import changeFavicon from "./utils/changeFavicon";
+import storageAvailable from "./localStorage/storageAvailable";
 import toggleLightMode from "./localStorage/lightMode";
 
 (() => {
   // changeFavicon
   changeFavicon(Logo);
-  // set lightMode
-  toggleLightMode();
+
+  // test localStorage availability
+  const localStorageAvailable = storageAvailable("localStorage");
+
+  if (localStorageAvailable === true) {
+    // set lightMode
+    toggleLightMode();
+  } else {
+    // log error in console && false silently with UI user
+    console.log(localStorageAvailable);
+  }
 })();
